580aad9994a2d1cf4c073f45824fc98f6132c5e7
unknown <n.t.sithy@gmail.com>
2025-02-16 02:35:51 +0600

    You have to write a function to print prime numbers from 1 to 100

---
+++ primenumber.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//write a function to print prime numbers from 1 to 100 and call it in the main function
+
+int main() {
+
+    //call the function here
+    return 0;
+}
