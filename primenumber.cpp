34c3b3fc2d892333eb4da959c84ca52888d75c55
kawchar-hossen <kawcharhossencd@gmail.com>
2025-02-23 09:51:39 +0600

    kawchar 221400055 have modified this file

--- primenumber.cpp
+++ primenumber.cpp
@@ -1,10 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//write a function to print prime numbers from 1 to 100 and call it in the main function
-//code
-int main() {
+// Function to check if a number is prime
+bool isPrime(int num) {
+    if (num < 2) return false;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
 
-    //call the function here
+// Function to print prime numbers from 1 to 100
+void printPrimes() {
+    for (int i = 1; i <= 100; i++) {
+        if (isPrime(i)) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    // Call the function here
+    printPrimes();
     return 0;
 }
